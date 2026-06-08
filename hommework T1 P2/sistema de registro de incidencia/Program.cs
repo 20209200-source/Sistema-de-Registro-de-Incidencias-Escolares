341b952cc5c2e95405b0376c7642febac92000b1
20209200-source <20209200@itla.edu.do>
2026-06-08 05:59:05 -0400

    adding first issue, closes #1

---
+++ hommework T1 P2/sistema de registro de incidencia/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SistemaRegistroIncidencias
+{
+    public class Estudiante
+    {
+    }
+    public class Profesor
+    {
+    }
+    public class Incidencia
+    {
+    }
+    public class EstudianteController
+    {
+    }
+    public class ProfesorController
+    {
+    }
+
+    public class IncidenciaController
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema de Registro de Incidencias Escolares");
+        }
+    }
+}
