e0036275b01eef5dd4c4f493aa2593e02695bae4
20209200-source <20209200@itla.edu.do>
2026-06-08 20:59:25 -0400

    adding second issue, closes #2

---
+++ hommework T1 P2/issue #2/issue #2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SistemaRegistroIncidencias
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Matricula { get; set; }
+        public string Curso { get; set; }
+    }
+
+    public class Profesor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Departamento { get; set; }
+    }
+
+    public class Incidencia
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string TipoIncidencia { get; set; }
+        public string AccionTomada { get; set; }
+
+        public int EstudianteId { get; set; }
+        public int ProfesorId { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entidades creadas correctamente.");
+        }
+    }
+}
