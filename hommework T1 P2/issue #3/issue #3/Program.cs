ae2409d55069eb9cfe35b9eda797d73276f8467d
20209200-source <20209200@itla.edu.do>
2026-06-10 07:02:33 -0400

    adding third issue, close #3

---
+++ hommework T1 P2/issue #3/issue #3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SistemaRegistroIncidencias
+{
+    // Controlador de Estudiantes
+    public class EstudianteController
+    {
+        public void RegistrarEstudiante()
+        {
+            Console.WriteLine("Estudiante registrado correctamente.");
+        }
+
+        public void ConsultarEstudiante()
+        {
+            Console.WriteLine("Consulta de estudiante realizada.");
+        }
+    }
+    public class ProfesorController
+    {
+        public void RegistrarProfesor()
+        {
+            Console.WriteLine("Profesor registrado correctamente.");
+        }
+
+        public void ConsultarProfesor()
+        {
+            Console.WriteLine("Consulta de profesor realizada.");
+        }
+    }
+    public class IncidenciaController
+    {
+        public void RegistrarIncidencia()
+        {
+            Console.WriteLine("Incidencia registrada correctamente.");
+        }
+
+        public void ConsultarIncidencias()
+        {
+            Console.WriteLine("Consulta de incidencias realizada.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EstudianteController estudiante = new EstudianteController();
+            ProfesorController profesor = new ProfesorController();
+            IncidenciaController incidencia = new IncidenciaController();
+
+            estudiante.RegistrarEstudiante();
+            profesor.RegistrarProfesor();
+            incidencia.RegistrarIncidencia();
+        }
+    }
+}
